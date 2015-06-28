bd1b26e2ae845fdba11b78fcd1ac6784f055a1ee
khalif cooper <khalifcooper@ymail.com>
2015-06-28 02:01:00 -0400

    added website

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,8 @@
-;(function (){
+// angular.module('myApp', [])
 
-  'use strict';
+// .controller('zero-issueCtrl', function($scope){
+//   $scope.image = "images/zero-issue.png";
 
-  angular.module('app', ['ngRoute'])
 
-  .config(['$routeProvider',
-    function($routeProvider){
+// });
 
-      $routeProvider.when('/', {
-        controller:   'Home',
-        templateUrl:  'js/home/home.tpl.html'
-      });
-
-   }]);
-
-}());
